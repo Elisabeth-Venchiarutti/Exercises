e2fdb5cc19f32a06e4cc03d07b954305c238211d
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-13 14:52:43 +0200

    Add files via upload

---
+++ ex 12 sept_7_angle.py
@@ -0,0 +1,32 @@
+﻿
+#! C:\Python34\python
+# -*- coding: UTF-8 -*# pour spécifier le codage des caractères
+
+
+
+from math import *
+import os
+
+hypo = (input ("Hypoténusa :  "))
+hypof = float(hypo)
+
+oppose = (input ("Côté opposé :  "))
+opposef = float(oppose)
+
+
+print("Angle : ", degrees (asin (opposef/hypof)))
+
+
+
+
+
+
+
+          
+
+
+
+
+          
+
+
