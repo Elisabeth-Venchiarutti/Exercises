90fef1afe0a5969729da10c3a3829d7fcfa9ac57
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-13 14:52:24 +0200

    Add files via upload

---
+++ ex 12 sept_6_chaine_nombre.py
@@ -0,0 +1,26 @@
+﻿
+#! C:\Python34\python
+# -*- coding: UTF-8 -*# pour spécifier le codage des caractères
+
+
+
+from math import *
+import os
+
+chaine = (input ("Tapez chaine de caractères :  "))
+
+nombre = (input ("tapez un nombre : "))
+resultat = chaine + "   " + nombre
+
+print(resultat)
+print (type(resultat))
+
+
+          
+
+
+
+
+          
+
+
