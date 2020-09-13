5c4b3db353470db1a34620e91d4bc1291255cb60
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-13 14:53:26 +0200

    Add files via upload

---
+++ donnees_elisa.py
@@ -0,0 +1,30 @@
+﻿"""les données pour le programme Pendu"""
+
+# nombre de coups par partie
+nb_coups = 8
+
+# Nom du ficher stockant les scores
+nom_fichier_scores = "scores"
+
+
+# Liste des mots du pendu
+liste_mots =[
+    "armoire",
+    "boucle",
+    "buisson",
+    "bureau",
+    "chaise",
+    "carton",
+    "couteau",
+    "fichier",
+    "garage",
+    "glace"
+    "journal",
+    "kiwi",
+    "lampe",
+    "liste",
+]
+
+
+
+    
