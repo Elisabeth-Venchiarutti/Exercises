dc73e8ccfb0a00fae85af483805e4cc1bb531539
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-13 14:51:22 +0200

    Add files via upload

---
+++ ex 12 sept_2_type.py
@@ -0,0 +1,24 @@
+﻿
+#! C:\Python34\python
+# -*- coding: UTF-8 -*# pour spécifier le codage des caractères
+
+
+
+var_texte = "Mon texte"
+var_ent = 5
+var_float = 10.5
+
+print("Texte = ", var_texte, "Type : ",type(var_texte))
+print("Entier = ", var_ent, "Type : ",type(var_ent))
+print("Float = ", var_float, "Type : ",type(var_float))
+
+chaine,entier,decimal = ("Mon_texte",5,10.5)
+
+print(chaine,entier,decimal)
+
+print (chaine, type(chaine))
+print (entier, type(entier))
+print (decimal, type(decimal))
+
+
+
