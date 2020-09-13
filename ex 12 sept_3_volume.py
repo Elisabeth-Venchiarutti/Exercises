da73ac03211779e007d8c44fd976aef4be049df7
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-13 14:51:49 +0200

    Add files via upload

---
+++ ex 12 sept_3_volume.py
@@ -0,0 +1,22 @@
+﻿
+#! C:\Python34\python
+# -*- coding: UTF-8 -*# pour spécifier le codage des caractères
+
+
+
+from math import *
+
+h = float (input ("Hauteur :  "))
+r = float (input ("Rayon :   "))
+
+V = 1/3 * pi * r**2 * h
+
+print("Volume =   ", V)
+
+
+
+
+
+          
+
+
