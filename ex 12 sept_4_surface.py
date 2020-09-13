4deab773dff67b496e5ab5a8e0f56e5b53691f59
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-13 14:52:07 +0200

    Add files via upload

---
+++ ex 12 sept_4_surface.py
@@ -0,0 +1,32 @@
+﻿
+#! C:\Python34\python
+# -*- coding: UTF-8 -*# pour spécifier le codage des caractères
+
+
+
+from math import *
+import os
+
+rExt = float (input ("Rayon ext :  "))
+rInt = float (input ("Rayon int :  "))
+
+if rInt >= rExt:
+    print ("Rayon int > Rayon ext")
+    print ("Fin d'exercice")
+    os.system("pause")
+
+else :
+    sGrandDisque = pi * rExt**2
+    sTrou = pi * rInt**2
+
+    surface = sGrandDisque - sTrou
+
+    print("Surface du disque découpé =   ", surface)
+
+
+
+
+
+          
+
+
