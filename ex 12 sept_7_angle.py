b9689f4040d545cbd90e4346de0960a27779483c
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-14 09:58:54 +0200

    Update ex 12 sept_7_angle.py

--- ex 12 sept_7_angle.py
+++ ex 12 sept_7_angle.py
@@ -2,6 +2,11 @@
 #! C:\Python34\python
 # -*- coding: UTF-8 -*# pour spécifier le codage des caractères
 
+"""
+programme, qui donne la mesure de l’angle  d’un triangle rectangle, dont on saisit le
+côté opposé et l’hypothénuse.
+Rappel : sinφ = CoteOppose/Hypotenuse
+"""
 
 
 from math import *
