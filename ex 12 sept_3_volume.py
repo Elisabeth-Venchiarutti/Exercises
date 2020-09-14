ea520769be5b456413d4351d3606b70d8291ea28
Elisabeth-Venchiarutti <70375863+Elisabeth-Venchiarutti@users.noreply.github.com>
2020-09-14 09:53:11 +0200

    Update ex 12 sept_3_volume.py

--- ex 12 sept_3_volume.py
+++ ex 12 sept_3_volume.py
@@ -2,7 +2,8 @@
 #! C:\Python34\python
 # -*- coding: UTF-8 -*# pour spécifier le codage des caractères
 
-
+"""programme qui, à partir de la saisie d’un rayon et d’une hauteur, calcule le volume d’un
+cône droit : V = 1/3* pi*r2 * h   """
 
 from math import *
 
